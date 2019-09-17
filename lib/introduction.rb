a1ee147d0496a6fa198c5af94e23db6457413e6e
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-17 03:51:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction_with_language(name,language)
 end
 
 def introduction_with_language_optional(name,language)
-  if language == "ruby"
+  if language == "Ruby"
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
 end
